c19e1a9c47ee482f9cff1a9d109939bbdcedb5f3
Abdulraham_Ahmed <abdulrahman.ahmed20072@gmail.com>
2024-12-07 19:56:44 +0200

    fix error

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings( BaseSettings ):
     
-    APP_NAME : str
-    APP_VERSION : str
+    APP_NAME: str
+    APP_VERSION: str
 
     class Config:
         env_file = ".env"
 
 def get_settings():
-    return Settings
+    return Settings()
