633fbdccd7af4c25b24137036c1585e5039d991d
Abdulraham_Ahmed <abdulrahman.ahmed20072@gmail.com>
2024-12-07 17:20:51 +0200

    edit base

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, APIRouter
-import os
+from helpers.config import get_settings
 
 base_router = APIRouter(
     prefix = "/api/v1",
@@ -9,8 +9,11 @@ base_router = APIRouter(
 
 @base_router.get("/")
 async def welcome():
-    app_name = os.getenv("APP_NAME")
-    app_ver = os.getenv("APP_VERSION")
+
+    app_settings = get_settings()
+
+    app_name = app_settings.APP_NAME
+    app_ver = app_settings.APP_VERSION
 
     return {
         "app_name" : app_name,
