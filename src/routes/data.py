714ee2aea3751ebb7e3551097be9abde4f1180ea
Abdulraham_Ahmed <abdulrahman.ahmed20072@gmail.com>
2024-12-22 01:17:36 +0200

    updaetd to use mongo

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI, APIRouter, UploadFile, Depends, status
+from fastapi import FastAPI, APIRouter, UploadFile, Depends, status, Request
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController, ProcessController
+from controllers import DataController, ProjectController, ProcessController, BaseController
 import aiofiles
 import os
 from models import ResponseSignal
 import logging
 from routes.schemes import ProcessRequest
+from models.ProjectModel import ProjectModel
+from models.ChunkModel import ChunkModel
+from models.db_schemes.data_chunk import DataChunk
+from models.enums import ResponseEnums
+# from bson.objectid import ObjectId
 
 logger = logging.getLogger("uvicorn.error")
 
@@ -16,9 +21,13 @@ data_router = APIRouter(
 )
 
 @data_router.post("/upload/{project_id}")
-async def upload_data( project_id: str , file: UploadFile,
+async def upload_data( request: Request, project_id: str , file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
     
+    project_model = ProjectModel( db_client = request.app.db_client )
+
+    project = await project_model.get_project_or_create_one(project_id)
+
     Data_Controller = DataController()
 
     is_valid, result_signal = Data_Controller.validate_uploaded_file( file = file )
@@ -29,7 +38,7 @@ async def upload_data( project_id: str , file: UploadFile,
             content = {"result_signal" : result_signal}
         )
     
-    project_dir_path = ProjectController().get_project_path(project_id = project_id)
+    # project_dir_path = ProjectController().get_project_path(project_id = project_id)
 
     file_path, file_id = Data_Controller.generate_unique_filepath(
         orig_file_name = file.filename , project_id = project_id)
@@ -49,16 +58,34 @@ async def upload_data( project_id: str , file: UploadFile,
 
     return JSONResponse(
             content = {"result_signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-                       "file_id" : file_id
+                       "file_id" : file_id, 
+                       "project_id": str(project.id),
                        }
         )
 
 @data_router.post("/process/{project_id}")
-async def process_endpoint(project_id: str, process_request: ProcessRequest):
+async def process_endpoint(request: Request, project_id: str, process_request: ProcessRequest):
     
+    project_model = ProjectModel( db_client = request.app.db_client )
+
+    project = await project_model.get_project_or_create_one(project_id)
+
     file_id = process_request.file_id
     chunk_size = process_request.chunk_size
     chunk_overlap = process_request.overlap_size
+    chunk_reset = process_request.do_reset
+
+    chunk_model = ChunkModel(db_client = request.app.db_client)
+
+    if chunk_reset == 1:
+        _ = await chunk_model.delete_chunks_by_project_id(project.id)
+
+    # check if the passed file is existed at project_id dir, i made it (^-^)
+    if not os.path.exists(os.path.join(BaseController().files_dir, f"{project_id}/{file_id}")):
+        return JSONResponse(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            content = {"result_signal" : ResponseSignal.FILE_PROCESS_NOT_FOUND.value}
+        )
 
     process_controller = ProcessController(project_id)
 
@@ -73,4 +100,18 @@ async def process_endpoint(project_id: str, process_request: ProcessRequest):
             content = {"result_signal" : ResponseSignal.FILE_PROCESS_FAILED.value}
         )
     
-    return file_chunks
+    file_chunks_records = [
+
+        DataChunk(
+            chunk_text = chunk.page_content,
+            chunk_metadata = chunk.metadata,
+            chunk_order = i+1,
+            chunk_project_id = project.id,
+        )
+
+        for i, chunk in enumerate(file_chunks)
+    ]
+
+    no_records = await chunk_model.insert_many_chunks(chunks = file_chunks_records)
+
+    return no_records
