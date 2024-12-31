5b09078bd93c3ae7127fdb08d1a7e953afe879b1
Abdulraham_Ahmed <abdulrahman.ahmed20072@gmail.com>
2024-12-31 20:12:50 +0200

    add func to get db path

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -10,9 +10,24 @@ class BaseController:
         self.app_settings = get_settings()
         
         self.base_dir = os.path.dirname(os.path.dirname(__file__))
-        self.files_dir = os.path.join(self.base_dir, "assets/files")
+
+        self.files_dir = os.path.join(
+            self.base_dir, "assets/files")
+        
+        self.database_dir = os.path.join(
+            self.base_dir, "assets/database")
 
     def generate_random_string(self, length: int = 12):
 
         # generating random strings
         return ''.join(random.choices(string.ascii_letters, k = length))
+    
+    def get_database_path(self, db_name):
+
+        database_path = os.path.join(
+            self.database_dir, db_name)
+        
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+        
+        return database_path
