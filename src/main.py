dbe4e927a7ce1dd401846b47c424d35daf73e30b
Abdulraham_Ahmed <abdulrahman.ahmed20072@gmail.com>
2025-01-08 18:02:11 +0200

    update main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
-from routes import base, data
+from routes import base, data, nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
-from .stores.llm import LLMProviderFactory
+from stores.llm import LLMProviderFactory
+from stores.vectordb import VectorDBProviderFactory
+from stores.llm.templates.template_parser import TemplateParser
 
 app = FastAPI()
 
-async def startup_db_client():
+@app.on_event("startup")
+async def startup_span():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient( settings.MONGODB_URL )
     app.db_client = app.mongo_conn[ settings.MONGODB_DATABASE ]
 
+    #factories
     llm_provider_factory = LLMProviderFactory(config = settings)
+    vectordb_provider_factory = VectorDBProviderFactory(config = settings)
 
     # generation client
     app.generation_client = llm_provider_factory.create(provider = settings.GENERATION_BACKEND)
@@ -22,12 +27,26 @@ async def startup_db_client():
     app.embedding_client.set_embedding_model(model_id = settings.EMBEDDING_MODEL_ID,
                                              embedding_size = settings.EMBEDDING_MODEL_SIZE)
 
-async def shutdown_db_client():
+    # vectordb client
+    app.vectordb_client = vectordb_provider_factory.create(
+        provider = settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
+
+    app.template_parser = TemplateParser(
+        language = settings.PRIMARY_LANG, 
+        default_language = settings.DEFAULT_LANG, 
+    )
+
+@app.on_event("shutdown")
+async def shutdown_span():
     app.mongo_conn.close()
+    app.vectordb_client.disconnect()
 
-app.router.lifespan.on_startup.append(startup_db_client)
-app.router.lifespan.on_shutdown.append(shutdown_db_client)
+# events
+# app.on_event("startup")(startup_span)
+# app.on_event("shutdown")(shutdown_span)
 
+#routers
 app.include_router( base.base_router )
-
 app.include_router( data.data_router )
+app.include_router( nlp.nlp_router )
