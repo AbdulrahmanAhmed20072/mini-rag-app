415885a13aea5c9655f71d4633db8a149f7e6e42
Abdulraham_Ahmed <abdulrahman.ahmed20072@gmail.com>
2025-01-14 18:41:43 +0200

    update validate_project_id name

--- src/models/db_schemes/project.py
+++ src/models/db_schemes/project.py
@@ -8,7 +8,7 @@ class Project(BaseModel):
     project_id: str = Field(min_length = 1)
 
     @field_validator("project_id")
-    def validate_projext_id(cls, value):
+    def validate_project_id(cls, value):
 
         if not value.isalnum():
             raise ValueError("project_id must be alphanumeric")
