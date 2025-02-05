f54f3078968b97edb498269c4d838765a88b97ee
Abdulraham_Ahmed <abdulrahman.ahmed20072@gmail.com>
2025-02-05 17:22:30 +0200

    update to add HuggingFace provider

--- src/stores/llm/LLMProviderFactory.py
+++ src/stores/llm/LLMProviderFactory.py
@@ -1,5 +1,5 @@
 from .LLMEnums import LLMEnums
-from .providers import OpenAIProvider, CoHereProvider
+from .providers import OpenAIProvider, CoHereProvider, HuggingFaceProvider
 
 class LLMProviderFactory:
 
@@ -20,6 +20,13 @@ class LLMProviderFactory:
         elif provider == LLMEnums.COHERE.value:
             return CoHereProvider(
                 api_key = self.config.COHERE_API_KEY,
+                default_input_max_characters = self.config.INPUT_DEFAULT_MAX_CHARACTERS,
+                default_generation_max_output_tokens = self.config.GENERATION_DEFAULT_MAX_TOKENS,
+                default_generation_temperature = self.config.GENERATION_DEFAULT_TEMPERATURE
+            )
+        
+        elif provider == LLMEnums.HUGGING_FACE.value:
+            return HuggingFaceProvider(
                 default_input_max_characters = self.config.INPUT_DEFAULT_MAX_CHARACTERS,
                 default_generation_max_output_tokens = self.config.GENERATION_DEFAULT_MAX_TOKENS,
                 default_generation_temperature = self.config.GENERATION_DEFAULT_TEMPERATURE
